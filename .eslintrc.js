eb947f9a5e636866f847182f9b0b953b10bedbb6
auooru <auooru@163.com>
2020-01-03 18:15:23 +0800

    调整 eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'comma-dangle': ["warn", "always-multiline"]
+    'comma-dangle': ["warn", "always-multiline"],
+    'camelcase': 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
