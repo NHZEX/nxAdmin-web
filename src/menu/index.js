13cc72f86d64cf9e49621554d903a53b6f264b56
auooru <auooru@outlook.com>
2020-03-24 01:07:20 +0800

    remove: demo pages

--- src/menu/index.js
+++ src/menu/index.js
@@ -4,15 +4,6 @@ import admin from './modules/admin'
 export const menuAside = [
   { path: '/index', title: '首页', icon: 'home' },
   ...admin,
-  {
-    title: '页面',
-    icon: 'folder-o',
-    children: [
-      { path: '/page1', title: '页面 1' },
-      { path: '/page2', title: '页面 2' },
-      { path: '/page3', title: '页面 3' },
-    ],
-  },
 ]
 
 // 菜单 顶栏
@@ -21,14 +12,5 @@ export const menuHeader = [
     path: '/index',
     title: '首页',
     icon: 'home',
-  },
-  {
-    title: '页面',
-    icon: 'folder-o',
-    children: [
-      { path: '/page1', title: '页面 1' },
-      { path: '/page2', title: '页面 2' },
-      { path: '/page3', title: '页面 3' },
-    ],
   },
 ]
