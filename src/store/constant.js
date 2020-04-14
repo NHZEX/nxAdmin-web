0120059f8931895ee268c7081384b0a91954a3b7
auooru <auooru@163.com>
2020-04-14 15:28:40 +0800

    update: constant.js

--- src/store/constant.js
+++ src/store/constant.js
@@ -1,3 +1,24 @@
+/**
+ * @param {Object} data
+ * @param {String} label
+ * @param {String} value
+ * @return {[]}
+ */
+export function toLabelValue (data, label = 'label', value = 'value') {
+  const result = []
+  for (const key in data) {
+    if (!Object.prototype.hasOwnProperty.call(data, key)) {
+      continue
+    }
+    const vv = Number(key)
+    result.push({
+      [label]: data[key],
+      [value]: Number.isNaN(vv) ? key : vv,
+    })
+  }
+  return result
+}
+
 export const ADMIN_USERS_GENRE = {
   1: '超级管理员',
   2: '管理员',
@@ -7,24 +28,4 @@ export const ADMIN_USERS_GENRE = {
 export const ADMIN_ROLES_GENRE = {
   1: '系统角色',
   2: '代理角色',
-}
-
-/**
- * @param {Object} data
- * @param {String} label
- * @param {String} value
- * @return {[]}
- */
-export function toLabelValue (data, label = 'label', value = 'value') {
-  let result = []
-  for (let key in data) {
-    if (!data.hasOwnProperty(key)) {
-      continue
-    }
-    result.push({
-      [label]: data[key],
-      [value]: key,
-    })
-  }
-  return result
 }
