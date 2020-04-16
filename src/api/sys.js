537a1a1edb9bbbf73cf3c45229799268607d5dd4
auooru <auooru@163.com>
2020-04-16 16:04:13 +0800

    update: 更新Api地址

--- src/api/sys.js
+++ src/api/sys.js
@@ -1,13 +1,13 @@
 import request, { baseUrl } from '@/plugin/axios'
 
 export function sysCongig () {
-  return request.get('/api.system/config')
+  return request.get('/api/system/config')
 }
 
 export function sysinfo () {
-  return request.get('/api.system/sysinfo')
+  return request.get('/api/system/sysinfo')
 }
 
 export const SYS_URLS = {
-  GET_CAPTCHA: `${baseUrl}api.system/captcha`,
+  GET_CAPTCHA: `${baseUrl}api/system/captcha`,
 }
--- src/api/sys.login.js
+++ src/api/sys.login.js
@@ -1,19 +1,19 @@
 import request from '@/plugin/axios'
 
 export function accountLogin (username, password, code, lasting, token) {
-  return request.post('/api.admin.index/login', {
-    'account': username,
-    'password': password,
-    'captcha': code,
-    'lasting': lasting,
-    'token': token,
+  return request.post('/api/admin/login', {
+    account: username,
+    password: password,
+    captcha: code,
+    lasting: lasting,
+    token: token,
   })
 }
 
 export function accountLogout () {
-  return request.get('/api.admin.index/logout')
+  return request.get('/api/admin/logout')
 }
 
 export function userInfo () {
-  return request.get('/api.admin.index/userInfo')
+  return request.get('/api/admin/user-info')
 }
