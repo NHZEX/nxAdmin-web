8e8bb717fcccdc277c96a1b188616129b60b1fd5
auooru <auooru@outlook.com>
2020-12-12 23:48:36 +0800

    update: permission drag

--- src/views/admin/permission.vue
+++ src/views/admin/permission.vue
@@ -6,6 +6,7 @@
       <el-button v-access="'admin.permission.edit'" type="success" :loading="loading.render" :disabled="!isChange" @click="saveChange()" icon="el-icon-upload">保存更改</el-button>
     </div>
     <vxe-grid
+      class="sortable-tree-demo"
       ref="tree"
       show-overflow
       row-key
@@ -37,6 +38,8 @@
 import adminPermissionView from './permission-view'
 import { getPermissions, savePermission, scanPermission } from '@api/admin/admin'
 import ContainerResize from '@/mixin/container-resize'
+import Sortable from 'sortablejs'
+import { findTree } from 'xe-utils'
 
 const recursionTree = (d) => {
   return d.map(el => {
@@ -61,6 +64,25 @@ export default {
         scan: false,
       },
       columns: [
+        {
+          width: 60,
+          slots: {
+            default: () => {
+              return [
+                <span class="drag-btn">
+                  <i class="vxe-icon--menu"/>
+                </span>
+              ]
+            },
+            header: () => {
+              return [
+                <vxe-tooltip v-model={ this.showHelpTip2 } content="按住后可以上下拖动排序！" enterable>
+                  <i class="vxe-icon--question" onClick={ () => { this.showHelpTip2 = !this.showHelpTip2 } }/>
+                </vxe-tooltip>
+              ]
+            }
+          }
+        },
         { title: '权限', field: 'title', treeNode: true, width: 350 },
         {
           title: '排序',
@@ -86,6 +108,7 @@ export default {
       ],
       data: [],
       isChange: false,
+      sortable2: null,
     }
   },
   methods: {
@@ -130,16 +153,95 @@ export default {
         this.load()
       })
     },
+    treeDrop () {
+      this.$nextTick(() => {
+        const xTable = this.$refs.tree
+        this.sortable2 = Sortable.create(xTable.$el.querySelector('.body--wrapper>.vxe-table--body tbody'), {
+          handle: '.drag-btn',
+          fallbackTolerance: 10,
+          onMove: function (evt, originalEvent) {
+            const draggRow = xTable.getRowNode(evt.dragged).item
+            const relatRow = xTable.getRowNode(evt.related).item
+            console.log(draggRow.name, relatRow.name, evt.willInsertAfter, draggRow.pid !== relatRow.pid)
+            if (draggRow.pid !== relatRow.pid) {
+              return false
+            }
+            if (evt.willInsertAfter) {
+              return -1
+            } else {
+              return true
+            }
+          },
+          onEnd: ({ item, oldIndex }) => {
+            const options = { children: 'children' }
+            const targetTrElem = item
+            const wrapperElem = targetTrElem.parentNode
+            const prevTrElem = targetTrElem.previousElementSibling
+            const tableTreeData = this.data
+            const selfRow = xTable.getRowNode(targetTrElem).item
+            const selfNode = findTree(tableTreeData, row => row === selfRow, options)
+            if (prevTrElem) {
+              // 移动到节点
+              const prevRow = xTable.getRowNode(prevTrElem).item
+              const prevNode = findTree(tableTreeData, row => row === prevRow, options)
+              // console.log(wrapperElem)
+              // console.log(selfRow.name, prevRow.name, selfNode, prevNode)
+              // console.log(selfRow.name, selfRow.pid, prevRow.name, prevRow.pid)
+              if (findTree(selfRow[options.children], row => prevRow === row, options)) {
+                // 错误的移动
+                const oldTrElem = wrapperElem.children[oldIndex]
+                wrapperElem.insertBefore(targetTrElem, oldTrElem)
+                return this.$message({ message: '不允许自己给自己拖动！', status: 'error' })
+              }
+              if (selfRow.pid !== prevRow.pid) {
+                // 错误的移动
+                const oldTrElem = wrapperElem.children[oldIndex]
+                wrapperElem.insertBefore(targetTrElem, oldTrElem)
+                return this.$message({ message: '不允许自己给自己拖动！', status: 'error' })
+              }
+              if (xTable.isTreeExpandByRow(prevRow)) {
+                // 移动到当前的子节点
+                const currRow = selfNode.items.splice(selfNode.index, 1)[0]
+                prevRow[options.children].splice(0, 0, currRow)
+              } else {
+                // 移动到相邻节点
+                const currRow = selfNode.items.splice(selfNode.index, 1)[0]
+                prevNode.items.splice(prevNode.index + (selfNode.index < prevNode.index ? 0 : 1), 0, currRow)
+              }
+            } else {
+              // 移动到第一行
+              var currRow = selfNode.items.splice(selfNode.index, 1)[0]
+              tableTreeData.unshift(currRow)
+            }
+            // 如果变动了树层级，需要刷新数据
+            xTable.syncData()
+          }
+        })
+      })
+    }
+  },
+  created () {
+    this.treeDrop()
+  },
+  beforeDestroy () {
+    if (this.sortable2) {
+      this.sortable2.destroy()
+    }
   },
   async mounted () {
     await this.$nextTick()
     await this.load()
   },
-  beforeDestroy () {
-  }
 }
 </script>
 
 <style scoped>
-
+.sortable-tree-demo .drag-btn {
+  cursor: move;
+  font-size: 12px;
+}
+.sortable-tree-demo .vxe-body--row.sortable-ghost,
+.sortable-tree-demo .vxe-body--row.sortable-chosen {
+  background-color: #dfecfb;
+}
 </style>
